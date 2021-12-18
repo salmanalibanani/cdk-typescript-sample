dc5e6f75219fa00bf9df130c1c1d23d6815f2986
Salman Ali Banani <salmanalibanani@yahoo.com>
2021-12-18 17:39:14 +1100

    Added noEmit to get rid of JavaScript files

--- lib/cdk-typescript-sample-stack.ts
+++ lib/cdk-typescript-sample-stack.ts
@@ -1,19 +1,31 @@
-import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
-import { Construct } from 'constructs';
+import { Duration, RemovalPolicy, Stack, StackProps } from "aws-cdk-lib";
+import * as sns from "aws-cdk-lib/aws-sns";
+import * as subs from "aws-cdk-lib/aws-sns-subscriptions";
+import * as sqs from "aws-cdk-lib/aws-sqs";
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
+
+import { Construct } from "constructs";
 
 export class CdkTypescriptSampleStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'CdkTypescriptSampleQueue', {
-      visibilityTimeout: Duration.seconds(300)
+    const table = new dynamodb.Table(this, "MyTable", {
+      partitionKey: {
+        name: "Id",
+        type: dynamodb.AttributeType.STRING,
+      },
+      tableName: "MyTable",
+      removalPolicy: RemovalPolicy.DESTROY,
     });
 
-    const topic = new sns.Topic(this, 'CdkTypescriptSampleTopic');
-
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    table.addGlobalSecondaryIndex({
+      indexName: "userIdIndex",
+      partitionKey: { name: "userId", type: dynamodb.AttributeType.STRING },
+      sortKey: { name: "status", type: dynamodb.AttributeType.STRING },
+      readCapacity: 1,
+      writeCapacity: 1,
+      projectionType: dynamodb.ProjectionType.ALL,
+    });
   }
 }
